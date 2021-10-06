5d299c686407140230c52c786860373527062aeb
Daryl Fong <darkare@gmail.com>
2021-10-06 22:00:45 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var SignalRJS = require('signalrjs');
+
+var signalR = SignalRJS();
+
+var server = express();
+server.use(signalR.createListener())
+server.use(express.static(__dirname));
+server.listen(3000);
+
+signalR.on('CONNECTED',function(){
+	console.log('connected');
+	setInterval(function () {
+		signalR.send({time:new Date()});
+	},1000)
+});
